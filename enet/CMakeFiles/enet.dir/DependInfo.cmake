0d425171d31cc3afb968dde840fad04710d0e524
Trish V Scott <x90265@Trishs-MacBook-Air.local>
2011-08-29 13:07:59 -0700

    eihrul's enet fixes and other server fixes

---
+++ enet/CMakeFiles/enet.dir/DependInfo.cmake
@@ -0,0 +1,26 @@
+# The set of languages for which implicit dependencies are needed:
+SET(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+SET(CMAKE_DEPENDS_CHECK_C
+  "/Users/x90265/Desktop/QServ/enet/callbacks.c" "/Users/x90265/Desktop/QServ/enet/CMakeFiles/enet.dir/callbacks.o"
+  "/Users/x90265/Desktop/QServ/enet/host.c" "/Users/x90265/Desktop/QServ/enet/CMakeFiles/enet.dir/host.o"
+  "/Users/x90265/Desktop/QServ/enet/list.c" "/Users/x90265/Desktop/QServ/enet/CMakeFiles/enet.dir/list.o"
+  "/Users/x90265/Desktop/QServ/enet/packet.c" "/Users/x90265/Desktop/QServ/enet/CMakeFiles/enet.dir/packet.o"
+  "/Users/x90265/Desktop/QServ/enet/peer.c" "/Users/x90265/Desktop/QServ/enet/CMakeFiles/enet.dir/peer.o"
+  "/Users/x90265/Desktop/QServ/enet/protocol.c" "/Users/x90265/Desktop/QServ/enet/CMakeFiles/enet.dir/protocol.o"
+  "/Users/x90265/Desktop/QServ/enet/unix.c" "/Users/x90265/Desktop/QServ/enet/CMakeFiles/enet.dir/unix.o"
+  "/Users/x90265/Desktop/QServ/enet/win32.c" "/Users/x90265/Desktop/QServ/enet/CMakeFiles/enet.dir/win32.o"
+  )
+SET(CMAKE_C_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+SET(CMAKE_TARGET_DEFINITIONS
+  "STANDALONE"
+  "HAS_SOCKLEN_T"
+  )
+
+# Targets to which this target links.
+SET(CMAKE_TARGET_LINKED_INFO_FILES
+  )
---
+++ enet/CMakeFiles/enet.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/enet.dir/host.o"
+  "CMakeFiles/enet.dir/list.o"
+  "CMakeFiles/enet.dir/callbacks.o"
+  "CMakeFiles/enet.dir/packet.o"
+  "CMakeFiles/enet.dir/peer.o"
+  "CMakeFiles/enet.dir/protocol.o"
+  "CMakeFiles/enet.dir/unix.o"
+  "CMakeFiles/enet.dir/win32.o"
+  "libenet.pdb"
+  "libenet.a"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang C)
+  INCLUDE(CMakeFiles/enet.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
